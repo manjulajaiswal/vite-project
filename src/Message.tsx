665788914f67a2d45eaa05707bb0a65aa4354ec5
manjula <manjulajaiswal023@gmail.com>
2024-09-29 18:21:55 +0530

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing (i.e.starting a new word with caps while declaring a function)
+function Message(){
+    //jsx not html , it gets converted to javascript
+    
+    const name = '';
+    if(name)
+ return <h1> Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
